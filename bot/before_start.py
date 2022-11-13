9b29d1106a19bd79154048735394fb2cfba4fa92
tgramvn <109977670+tgramvn@users.noreply.github.com>
2022-11-13 19:48:41 +0700

    Update before_start.py

--- bot/before_start.py
+++ bot/before_start.py
@@ -21,7 +21,7 @@ async def fetch_admins(bot: Bot) -> Dict:
 async def check_rights_and_permissions(bot: Bot, chat_id: int):
     chat_member_info = await bot.get_chat_member(chat_id=chat_id, user_id=bot.id)
     if not isinstance(chat_member_info, ChatMemberAdministrator):
-        raise PermissionError("Bot is not an administrator")
+        raise PermissionError("Bot không phải là quản trị viên")
     if not chat_member_info.can_restrict_members or not chat_member_info.can_delete_messages:
-        raise PermissionError("Bot needs 'restrict participants' and 'delete messages' permissions to work properly")
+        raise PermissionError("Bot cần các quyền 'hạn chế người tham gia' và 'xóa tin nhắn' để hoạt động bình thường")
 
